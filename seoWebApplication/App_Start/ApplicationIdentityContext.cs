0e251cce15a640cf60eae75864441ae198fc5a86
Ty Moodley <ty.moodley@gmail.com>
2014-08-17 11:07:01 -0400

    added asp.net identity for mongo and CORS

---
+++ seoWebApplication/App_Start/ApplicationIdentityContext.cs
@@ -0,0 +1,36 @@
+﻿namespace seoWebApplication
+{
+	using System;
+	using AspNet.Identity.MongoDB;
+	using MongoDB.Driver;
+    using System.Configuration;
+
+	public class ApplicationIdentityContext : IdentityContext, IDisposable
+	{ 
+        static string connectionString = ConfigurationManager.ConnectionStrings["MongoDB"].ConnectionString;
+         
+		public ApplicationIdentityContext(MongoCollection users, MongoCollection roles) : base(users, roles)
+		{
+		}
+
+		public static ApplicationIdentityContext Create()
+		{
+			// todo add settings where appropriate to switch server & database in your own application
+
+            MongoUrl con = new MongoUrl(connectionString);
+            MongoClient client = new MongoClient(con);
+            MongoServer mongoServer = client.GetServer();
+            MongoDatabase database = mongoServer.GetDatabase("mongocommerce"); 
+
+            var users = database.GetCollection<IdentityUser>("users");
+            var roles = database.GetCollection<IdentityRole>("roles");
+			return new ApplicationIdentityContext(users, roles);
+
+            
+		}
+
+		public void Dispose()
+		{
+		}
+	}
+}
