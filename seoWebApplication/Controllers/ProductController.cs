35cf60cb4a6375257f5cbf8798a397a145d198d2
Ty Moodley <ty.moodley@gmail.com>
2014-08-18 22:52:39 -0400

    Added Product Service

---
+++ seoWebApplication/Controllers/ProductController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using seoWebApplication.Data;
+using seoWebApplication.Service;
+
+namespace seoWebApplication.Controllers
+{
+    public class ProductController : Controller
+    {
+        private SeoWebAppEntities db = new SeoWebAppEntities();
+        private ProductService _productService = new ProductService();
+        // GET: /Product/
+        public ActionResult Index()
+        {
+            foreach (seoWebApplication.Data.product prod in db.products)
+            {
+                Models.Products prods = new Models.Products();
+                prods.product_id = prod.product_id;
+                prods.name = prod.name;
+                prods.description = prod.description;
+                prods.defaultAttCat = prod.defaultAttCat;
+                prods.defaultAttribute = prod.defaultAttribute;
+                prods.image = prod.image;
+                prods.price = prod.price;
+                prods.promodept = prod.promodept;
+                prods.promofront = prod.promofront;
+                prods.thumbnail = prod.thumbnail;
+                prods.webstore_id = prod.webstore_id;
+                _productService.Create(prods);
+            }
+            return View(db.products.ToList());
+        }
+
+        // GET: /Product/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            seoWebApplication.Data.product product = db.products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: /Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Product/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "product_id,webstore_id,name,description,price,thumbnail,image,promofront,promodept,defaultAttribute,defaultAttCat,InsertDate,InsertENTUserAccountId,UpdateDate,UpdateENTUserAccountId,Version,IsSpecial")] seoWebApplication.Data.product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+
+        // GET: /Product/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            seoWebApplication.Data.product product = db.products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: /Product/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "product_id,webstore_id,name,description,price,thumbnail,image,promofront,promodept,defaultAttribute,defaultAttCat,InsertDate,InsertENTUserAccountId,UpdateDate,UpdateENTUserAccountId,Version,IsSpecial")] seoWebApplication.Data.product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        // GET: /Product/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            seoWebApplication.Data.product product = db.products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: /Product/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            seoWebApplication.Data.product product = db.products.Find(id);
+            db.products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ seoWebApplication/Service/ProductService.cs
@@ -0,0 +1,70 @@
+﻿using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using seoWebApplication.DAL;
+using seoWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace seoWebApplication.Service
+{
+    public class ProductService
+    {
+        private readonly MongoHelper<Products> _product;
+         public ProductService()
+        {
+            _product = new MongoHelper<Products>();
+        }
+
+         public void Create(Products product)
+        {
+            _product.Collection.Insert(product);
+        }
+
+
+         public IList<Products> GetProducts()
+        {
+            try
+            {
+                return _product.Collection.FindAll().ToList<Products>();
+            }
+            catch (MongoConnectionException)
+            {
+                return new List<Products>();
+            }
+        }
+
+         public IList<Products> GetProductsByDepartment(int Id)
+         {
+             try
+             {
+                 return _product.Collection.Find(Query.EQ("department_id", Id)).ToList<Products>();
+                  
+             }
+             catch (MongoConnectionException)
+             {
+                 return new List<Products>();
+             }
+         }
+
+         public IList<Products> GetProductsByCategory(int Id)
+         {
+             try
+             {
+                 return _product.Collection.Find(Query.EQ("category_id", Id)).ToList<Products>();
+
+             }
+             catch (MongoConnectionException)
+             {
+                 return new List<Products>();
+             }
+         }
+
+         public Products GetProducts(string name)
+        {
+            var post = _product.Collection.Find(Query.EQ("Name", name)).Single(); 
+            return post;
+        }
+    }
+}
