d968f627aa7b3f0bd1c1b2323454602c623e41bd
Ty Moodley <ty.moodley@gmail.com>
2014-10-12 09:20:42 -0400

    added new js to fix menu issues

--- seoWebApplication/js/theme.js
+++ seoWebApplication/js/theme.js
@@ -1,18 +1,21 @@
-﻿$("#mk-header-login-button").click(function () {
+﻿$("#mk-header-login-button").mouseover(function () {
+    $(this).addClass('active');
     $('#mk-header-subscribe').hide(); 
     $('#mk-nav-search-wrapper').hide();
     $('.mk-login-register').show();
     $('.mk-shopping-cart-box').hide();
 });
 
-$("#mk-header-subscribe-button").click(function () {
+$("#mk-header-subscribe-button").mouseover(function () {
+    $(this).addClass('active');
     $('.mk-login-register').hide(); 
     $('#mk-nav-search-wrapper').hide();
     $('#mk-header-subscribe').show(); 
     $('.mk-shopping-cart-box').hide();
 });
 
-$(".mk-search-trigger").click(function () {
+$(".mk-search-trigger").mouseover(function () {
+    $(this).addClass('active');
     $('.mk-login-register').hide();
     $('#mk-header-subscribe').hide();
     $('#mk-nav-search-wrapper').show();
@@ -20,9 +23,17 @@ $(".mk-search-trigger").click(function () {
 });
 
 $(".shoping-cart-link").mouseover(function () {
+    $(this).addClass('active');
     $('.mk-login-register').hide();
     $('#mk-header-subscribe').hide();
     $('#mk-nav-search-wrapper').hide();
     $('.mk-shopping-cart-box').show();
+});
+
+$("#home-bg").mouseover(function () { 
+    $('.mk-login-register').hide();
+    $('#mk-header-subscribe').hide();
+    $('#mk-nav-search-wrapper').hide();
+    $('.mk-shopping-cart-box').hide();
 });
 
