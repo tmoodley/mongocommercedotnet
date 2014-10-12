a3f6b0bfc79203f41131fafb51110bd36e586904
Ty Moodley <ty.moodley@gmail.com>
2014-10-11 22:45:20 -0400

    login

--- seoWebApplication/default2.master.designer.cs
+++ seoWebApplication/default2.master.designer.cs
@@ -58,6 +58,15 @@ namespace seoWebApplication {
         protected global::seoWebApplication.UserControls.CartSummary CartSummary;
         
         /// <summary>
+        /// LogIn control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::seoWebApplication.UserControls.LogIn LogIn;
+        
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
